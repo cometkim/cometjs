01794b6ee01d41fa700be884512532c7e32ae79a
Hyeseong Kim <cometkim.kr@gmail.com>
2020-07-25 05:55:54 +0900

    feat(react-utils): replace common utility types by DT's

--- packages/react-utils/src/common.ts
+++ packages/react-utils/src/common.ts
@@ -4,13 +4,15 @@ import type { OverrideProps } from '@cometjs/core';
 /**
  * Infer possible prop types of `typeof MyComponent`.
  *
+ * Has been replaced by DefinitelyTyped.
+ *
  * @example
  * ```tsx
  * import MyComponent from './MyComponent';
  * type MyComponentProps = PropsOf<typeof MyComponent>;
  * ```
  */
-export type PropsOf<TComponent> = TComponent extends React.ComponentType<infer P> ? P : never;
+export type PropsOf<TComponent extends React.ElementType> = React.ComponentProps<TComponent>
 
 /**
  * Infer a single prop type of `typeof MyComponent`.
@@ -21,12 +23,12 @@ export type PropsOf<TComponent> = TComponent extends React.ComponentType<infer P
  * type MyComponentChangeHandler = PropOf<typeof MyComponent, 'onChange'>;
  * ```
  */
-export type PropOf<TComponent, TKey extends keyof PropsOf<TComponent>> = PropsOf<TComponent>[TKey];
+export type PropOf<TComponent extends React.ElementType, TKey extends keyof PropsOf<TComponent>> = PropsOf<TComponent>[TKey];
 
 /**
  * Infer possible ref type of `typeof MyComponent`.
  *
- * @deprecated DefinitelyTyped now supports for this 🎉
+ * Has been replaced by DefinitelyTyped.
  * @see https://github.com/DefinitelyTyped/DefinitelyTyped/pull/43201
  *
  * @example
@@ -40,13 +42,7 @@ export type PropOf<TComponent, TKey extends keyof PropsOf<TComponent>> = PropsOf
  * <MyComponent ref={ref}/>
  * ```
  */
-export type RefOf<TRefForwardingComponent> = (
-  TRefForwardingComponent extends React.ForwardRefExoticComponent<infer TProps>
-  ? TProps extends { ref?: React.Ref<infer TRef> }
-  ? TRef
-  : never
-  : never
-);
+export type RefOf<TRefForwardingComponent extends React.ElementType> = React.ElementRef<TRefForwardingComponent>;
 
 /**
  * Safely override exist prop types
