36ae0dfe0316f498ee642706fdee44ad55bceba7
Hyeseong Kim <hey@hyeseong.kim>
2022-03-10 08:16:03 +0900

    add lcov data

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    coverage: {
+      reporter: [
+        'text',
+        'json',
+        'html',
+        'lcovonly',
+      ],
+    },
     exclude: [
       'packages/archive/**',
     ],
